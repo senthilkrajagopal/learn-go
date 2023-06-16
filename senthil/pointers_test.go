5747b4319b8a5d5d64bfe1100515846392f572d8
senrajag <senrajag@cisco.com>
2023-06-16 12:47:11 -0700

    pointers_test completed the test case

--- senthil/pointers_test.go
+++ senthil/pointers_test.go
@@ -1,1 +1,15 @@
 package senthil
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserNameChange(t *testing.T) {
+	u := user{
+		name: "mukund",
+	}
+	u.setName("mukund change")
+	assert.Equal(t, u.getName(), "mukund change")
+}
